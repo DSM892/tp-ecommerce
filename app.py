9f3e98b09c242a9892e9ac619ca13455a32acfaf
Dayssem SASSI <sassidayssem@gmail.com>
2026-03-18 10:09:20 +0100

    Etape 2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from database import DatabaseManager
 
 app = Flask(__name__)
+db = DatabaseManager()
 
 @app.route('/')
 def accueil():
@@ -8,11 +10,7 @@ def accueil():
 
 @app.route('/catalogue')
 def catalogue():
-    produits = [
-        {"id" : 1, "nom" : "T-shirt", "prix" : 25.99, "description" : "Coton bio"},
-        {"id" : 2, "nom" : "Jean", "prix" : 59.99, "description" : "Coupe slim"},
-        {"id" : 3, "nom" : "Casquette", "prix" : 15.00, "description" : "Style urbain"}
-    ]
+    produits = db.get_produits()
     return render_template('catalogue.html', produits=produits)
 
 if __name__ == '__main__':
