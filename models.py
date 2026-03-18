c63df4686821192cf0789f3aea0a786e6ea8d601
Nathan <ecole89000@outlook.com>
2026-03-18 11:24:17 +0100

    app.py

--- models.py
+++ models.py
@@ -16,16 +16,16 @@ class produit:
     def __str__(self):
         return f"{self.nom} - {self.prix} €"
     
-    class utilisateur:
-        def __init__(self, id, nom, email, mot_de_passe, role='client'):
-            self.id = id
-            self.nom = nom
-            self.email = email
-            self.mot_de_passe_hash = self.hasher(mot_de_passe)
-            self.role = role
+class utilisateur:
+    def __init__(self, id, nom, email, mot_de_passe, role='client'):
+        self.id = id
+        self.nom = nom
+        self.email = email
+        self.mot_de_passe_hash = self.hasher(mot_de_passe)
+        self.role = role
     @staticmethod
     def hasher(mdp):
         return hashlib.sha256(mdp.encode('utf-8')).hexdigest()
 
     def verifier_mdp(self, mdp_saisi):
-    
+        return self.mot_de_passe_hash == self.hasher(mdp_saisi)
